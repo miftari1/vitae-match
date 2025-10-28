7c0ce0465377412bd0944703ca4ec0b8619cb8d6
miftari1 <miftarib@hotmail.com>
2025-10-28 22:57:52 +0100

    Moved token router to separate file

---
+++ backend/app/routers/token.py
@@ -0,0 +1,32 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlmodel import Session
+from starlette import status
+
+from ..auth import authenticate_user, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
+from ..database import get_session
+from ..models import Token
+
+
+router = APIRouter(tags=["Token"])
+
+router.post("/token", response_model=Token)
+async def login_for_access_token(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    session: Annotated[Session, Depends(get_session)],
+):
+    user = authenticate_user(session, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
